3ace3e5117268276bb82963f14fbe048e9304117
evercadia <zlucard@hotmail.com>
2023-04-22 01:04:35 -0400

    testing C# and Github

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("Enter your numeric grade:");
+int grade = int.Parse(Console.ReadLine());
+
+string letterGrade;
+if (grade >= 90) {
+    letterGrade = "A";
+} else if (grade >= 80) {
+    letterGrade = "B";
+} else if (grade >= 70) {
+    letterGrade = "C";
+} else if (grade >= 60) {
+    letterGrade = "D";
+} else {
+    letterGrade = "F";
+}
+
+Console.WriteLine("Your letter grade is: " + letterGrade);
+
     }
 }
