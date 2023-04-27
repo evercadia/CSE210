5cd65c66fb20126e62af4ecb208c422f92f6db9c
evercadia <zlucard@hotmail.com>
2023-04-27 00:19:20 -0400

    penguin guess game , lists, and numbers

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("How many penguins would you like to guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Need more penguins");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Too many penguins!!");
+            }
+            else
+            {
+                Console.WriteLine("The perfect amount of penguins");
+                Console.Write("Would you like to play again? (y/n) ");
+                string playAgain = Console.ReadLine();
+                if (playAgain == "y")
+                {
+                    magicNumber = randomGenerator.Next(1, 101);
+                    guess = -1;
+                }
+                else
+                {
+                    Console.WriteLine("Thanks for playing!");
+                }
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,45 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int num;
+
+        do
+        {
+            Console.Write("Enter a number (or 0 to stop): ");
+            num = int.Parse(Console.ReadLine());
+            if (num != 0)
+            {
+                numbers.Add(num);
+            }
+        } while (num != 0);
+
+        int sum = 0;
+        int largest = numbers[0];
+
+        foreach (int n in numbers)
+        {
+            sum += n;
+            if (n > largest)
+            {
+                largest = n;
+            }
+        }
+
+        double average = (double)sum / numbers.Count;
+
+        Console.WriteLine("You entered:");
+        foreach (int n in numbers)
+        {
+            Console.Write(n + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("Sum: " + sum);
+        Console.WriteLine("Average: " + average);
+        Console.WriteLine("Largest number: " + largest);
     }
 }
