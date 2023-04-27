e0e6caf75ac537e2791a02b7cd3f8898e09ec36c
evercadia <zlucard@hotmail.com>
2023-04-27 00:23:17 -0400

    intro to functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+
+        int number = PromptUserNumber();
+
+        int squared = SquareNumber(number);
+
+        DisplayResult(name, squared);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int n)
+    {
+        return n * n;
+    }
+
+    static void DisplayResult(string name, int squared)
+    {
+        Console.WriteLine("Hi, " + name + "! Your favorite number squared is " + squared + ".");
     }
 }
